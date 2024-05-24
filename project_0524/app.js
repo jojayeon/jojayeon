23343989a8787a3e873fe3f14c4145662f781e57
jojayeon <166187873+jojayeon@users.noreply.github.com>
2024-05-24 18:13:31 +0900

    Add files via upload

---
+++ project_0524/app.js
@@ -0,0 +1,11 @@
+const http = require("http")
+const fs = require("fs")
+const path = rerquire('path')
+
+const sever = http.createServer((req,res)=>{
+  if(req.method === "GET") {
+    if(req.url === "/"){
+      
+    }
+  }
+})
